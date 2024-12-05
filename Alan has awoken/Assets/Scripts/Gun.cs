ce819fc920efddf133bf923c6118a5f214ef7186
JoaquinALTF4 <165952600+JoaquinALTF4@users.noreply.github.com>
2024-12-05 01:39:37 -0600

    final checkpoint #2

--- Alan has awoken/Assets/Scripts/Gun.cs
+++ Alan has awoken/Assets/Scripts/Gun.cs
@@ -11,15 +11,27 @@ public class Gun : MonoBehaviour
     public float gunDamage = 10f;
     public ParticleSystem muzzleFlash;
     public ParticleSystem impactEffect;
-  
+    public AudioClip gunSound;
+    public AudioSource source;
+    public GameObject AudioClip;
+    public void Start()
+    {
+        AudioClip.SetActive(false);
+    }
 
-    
-    
-       void Update()
+
+
+
+    void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
+            
             fireGun();
+            AudioClip.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(gunSound);
+            //I was trying to create the audio replaying every time you shot the gun
+
         }
     }
     void fireGun()
@@ -31,20 +43,20 @@ public class Gun : MonoBehaviour
         {
             //Debug.Log("Hit something");
 
-        
+
             // Debug.Log(hit.transform.name);
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)* hit.distance,Color.red);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
             Enemy darkness = hit.transform.GetComponent<Enemy>();
-       
+
             if (darkness != null)
 
             {
-                
+
                 darkness.enemytakeDamage(gunDamage);
             }
-            else
-            {
-                
+            else {
+            
+
             }
         }
 
